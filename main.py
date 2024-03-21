d830b8b28fb98dec574a165cf8b1d05ef6fa5494
kriswen <wen.shaoju@gmail.com>
2024-03-20 23:04:07 -0700

    refactor logics

--- main.py
+++ main.py
@@ -38,25 +38,22 @@ resources = {
 def print_report():
     """print the report of ingredients and money received"""
     print("#####################################")
-    print(f"Water: {resources['water']}ml")
     print(f"Milk: {resources['milk']}ml")
     print(f"Coffee: {resources['coffee']}g")
+    print(f"Water: {resources['water']}ml")
     print(f"Money: ${round(money_available, 2)}")
     print("#####################################")
 
 
 # TODO: 2. check resources sufficient?
-def check_resources(coffee):
-    """take a drink type and return insufficient resources (if any) in a list"""
+def check_insufficient_resources(coffee):
+    """take a drink type and return insufficient resources (if any) in a list format"""
     depleted_resources = []
     ingredients_needed = MENU[coffee]["ingredients"]
     # print(ingredients_needed)
-    for key, value in ingredients_needed.items():
-        # print(f"need: {key} {value}")
-        # print(f"resource for {key}: {resources[key]}")
-        # add ingredient name to depleted list if not sufficient
-        if int(resources[key]) < value:
-            depleted_resources.append(key)
+    for item in ingredients_needed:
+        if ingredients_needed[item] > int(resources[item]):
+            depleted_resources.append(item)
     # print(f"depleted list: {depleted_resources}")
     return depleted_resources
 
@@ -76,19 +73,29 @@ def process_coins():
 # TODO: 4. check transaction successful?
 def check_transaction(money, coffee):
     """Take the money and drink as input, and return True/False if it can cover the cost"""
+    global money_available
     drink_cost = MENU[coffee]["cost"]
-    print(f"The {coffee} cost ${drink_cost} and we received ${money} from you.")
-    return money >= drink_cost
+    if money >= drink_cost:
+        print(f"The {coffee} cost ${drink_cost} and we received ${money} from you.")
+        # process refund for the extra money received
+        refund_amount = round(money_received - MENU[coffee_type]["cost"], 2)
+        if refund_amount > 0:
+            print(f"Here is ${refund_amount} dollars in change.")
+        # bank the money if money is enough to cover the cost of drink & refund processed
+        money_after_refunded = money_received - refund_amount
+        money_available += round(money_after_refunded, 2)
+        return True
+    else:
+        print("Sorry that's not enough money. Money refunded.")
+        return False
 
 
 # TODO: 5. make coffee
 def make_coffee(coffee):
-    # deduct ingredients from the resources
+    """deduct ingredients from the resources"""
     ingredients_needed = MENU[coffee]["ingredients"]
-    for key, value in ingredients_needed.items():
-        resources[key] -= value
-        # print(f"deducting {value} of {key}")
-    # print the coffee
+    for item in ingredients_needed:
+        resources[item] -= ingredients_needed[item]
     print(f"Here is your {coffee} ☕, enjoy!")
 
 
@@ -112,33 +119,25 @@ print_logo_menu()
 
 while coffee_machine_on:
     user_input = input(" What would you like? (espresso/latte/cappuccino): ")
-    if user_input == "report":
-        print_report()
-    elif user_input == "off":
+    if user_input == "off":
         # turn off coffee machine (exit out the program)
         print("Coffee machine is turning off...")
         coffee_machine_on = False
-    elif user_input == "expresso" or "latte" or "cappuccino":
+    elif user_input == "report":
+        print_report()
+    elif user_input in ["espresso", "latte", "cappuccino"]:
         coffee_type = user_input
-        # check if depleted_resource list is empty
-        if not check_resources(coffee_type):
+        # if there is any insufficient resource, list them out
+        if check_insufficient_resources(coffee_type):
+            depleted_items = " & ".join(check_insufficient_resources(coffee_type))
+            print(f"Sorry there is not enough {depleted_items}.")
+        else:
             # process coins if sufficient resources
             money_received = round(process_coins(), 2)
             # check if money is enough to cover the drink cost
             if check_transaction(money_received, coffee_type):
-                # process refund for the extra money received
-                refund_amount = round(money_received - MENU[coffee_type]["cost"], 2)
-                if refund_amount > 0:
-                    print(f"Here is ${refund_amount} dollars in change.")
-                # bank the money if money is enough to cover the cost of drink & refund processed
-                money_after_refunded = money_received - refund_amount
-                money_available += round(money_after_refunded, 2)
                 # make the coffee
                 make_coffee(coffee_type)
-            else:
-                print("Sorry that's not enough money. Money refunded.")
-
-        # if there is any insufficient resource, list them out
-        else:
-            depleted_items = " & ".join(check_resources(coffee_type))
-            print(f"Sorry there is not enough {depleted_items}.")
+            # else do nothing
+    else:
+        print("Invalid input. Please try again.")
